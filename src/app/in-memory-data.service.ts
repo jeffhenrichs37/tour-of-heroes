aabd9d97ad3d2e2f565aced064daaaff682be862
Jeff Henrichs <jeffhenrichs37@gmail.com>
2020-12-18 11:31:44 -0500

    Installed In-memory Web API, added two new modules to ad.module.ts, and created a new service call in-memory-data.

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+import { HeroesComponent } from './heroes/heroes.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+
+  constructor() { }
+
+  createDb() {
+    const heroes = [
+      { if: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magenta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynma' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma'},
+      { id: 20, name: 'Tornado' }
+    ];
+    return {heroes};
+  }
+
+  /*
+    Override the genId methods to ensure that a hero always has an id.
+    If the heroes array is empty,
+    the method below return the inital number (11).
+    if the heroes array is not empty, the method below return the highest
+    hero id + 1
+  */
+ genId(heroes: Hero[]): number {
+   return HeroesComponent.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+ }
+}
